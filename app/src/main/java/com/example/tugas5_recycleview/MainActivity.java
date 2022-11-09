344f503260ed6bcec751989ec8c520d1eb74324c
EkaPradanahartinningrum <eka4833@gmail.com>
2022-11-09 13:23:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas5_recycleview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.tugas5_recycleview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+    RecyclerView recyclerView;
+    String s1[], s2[],s3[];
+    int images[] = {R.drawable.soekarno,R.drawable.soeharto,R.drawable.jokowi};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abdt = new
+                ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abdt);
+
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view =
+                (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_presiden) {
+                    Intent a = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_provinsi) {
+                    Intent a = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_bantuan) {
+                    Intent a = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(a);
+                }
+                return true;
+            }
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.presiden);
+        s2 = getResources().getStringArray(R.array.julukan);
+        s3 = getResources().getStringArray(R.array.deskripsi);
+        PresidenAdapter appAdapter = new PresidenAdapter(this, s1, s2, s3, images);
+        recyclerView.setAdapter(appAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));}
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abdt.onOptionsItemSelected(item) ||
+                super.onOptionsItemSelected(item);
+    }
+}
